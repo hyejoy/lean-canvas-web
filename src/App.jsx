1886ab51ad0affba89e90cb027788812f311e58e
hyejoy <devchoi97@gmail.com>
2026-01-01 16:34:12 +0900

    feat : React Router 기본 설정 및 nested route 구조 학습
    - What:
     - react-router-dom을 설치하고 createBrowserRouter로 라우터 객체를
       생성하여 URL경로에 따라 컴포넌트를 렌더링하도록 구성
       - children 기반 nested router와 Outlet을 활용한 화면분기 구현
    - Why:
     - 클라이언트 사이드 라우팅 개념을 이해하고, 공통 레이아웃(App)과 URL에
       따라 교체되는 영역을 분리하는 구조를 학습하기 위함
    - Notes:
     - RouterProvider를 통해 라우터를 전역에 연결하고, children 컴포넌트가
       Outlet 위치에 렌더링되는 흐름을 중심으로 학습

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import "./App.css";
-import Card1 from "./components/CssModules/Card1";
-import Card2 from "./components/CssModules/Card2";
+import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
     <>
-      <Card1 />
-      <hr />
-      <Card2 />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        {/* react-router에서 제공하는 컴포넌트로, 라우트 경로를 설정한 children 컴포넌트가 렌더링됨 */}
+        <Outlet />
+      </ul>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./MaterialUI.jsx";
+import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children에는 router 정보를 설정해주면됨
+    // children으로 설정된 컴포넌트가 상위 컴포넌트인 App컴포넌트의
+    // <Outlet/> 위치에 랜더링됨
+    children: [
+      {
+        //children의 path는 상위 컴포넌트 패스에 이어지기 때문에 상대경로로 설정해야함
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
